3dc09c53a20c50460d80a1cdb4d6727552d82629
Romain Gonord <romain.gonord.opensource@neteyes.org>
2014-09-27 14:30:25 +0200

    prepare login feature

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,8 +22,24 @@ angular.module('tropleindoApp').config(function ($routeProvider) {
 			templateUrl: 'views/details.html',
 			controller: 'DetailsCtrl'
 		})
+		.when('/login', {
+			templateUrl: 'views/login.html',
+			controller: 'LoginCtrl',
+			access: {
+				restricted: false
+			}
+		})
 		.otherwise({
 			redirectTo: '/'
 		})
 	;
+});
+
+angular.module('tropleindoApp').run(function ($rootScope, $location, $log, userService) {
+	$rootScope.$on('$locationChangeStart', function (scope, currView) {
+		if (!userService.isLogged() && (!currView || !currView.access || currView.access.restricted)) {
+			$log.log('user is not logged and access is restricted : redirecting to personal settings (/login)');
+			$location.path('/login');
+		}
+	});
 });
