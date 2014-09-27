37ee7b24b89f7e25745710f99fa13a3b55c9bd2e
Romain Gonord <romain.gonord.opensource@neteyes.org>
2014-09-27 23:01:24 +0200

    remove login feature ='( damn you github for not supporting full client side Oauth ='(

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,8 @@ angular.module('tropleindoApp', [
 	'ngResource',
 	'ngSanitize',
 	'ngRoute',
-	'angularCharts'
+	'angularCharts',
+	'ngStorage'
 ]);
 
 angular.module('tropleindoApp').config(function ($routeProvider) {
@@ -22,24 +23,8 @@ angular.module('tropleindoApp').config(function ($routeProvider) {
 			templateUrl: 'views/details.html',
 			controller: 'DetailsCtrl'
 		})
-		.when('/login', {
-			templateUrl: 'views/login.html',
-			controller: 'LoginCtrl',
-			access: {
-				restricted: false
-			}
-		})
 		.otherwise({
 			redirectTo: '/'
 		})
 	;
-});
-
-angular.module('tropleindoApp').run(function ($rootScope, $location, $log, userService) {
-	$rootScope.$on('$locationChangeStart', function (scope, currView) {
-		if (!userService.isLogged() && (!currView || !currView.access || currView.access.restricted)) {
-			$log.log('user is not logged and access is restricted : redirecting to personal settings (/login)');
-			$location.path('/login');
-		}
-	});
 });
